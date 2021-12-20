9d528065de70695a195fc0aef8b3727bb793d436
guyi-maple <guyi@guyi-maple.top>
2021-12-20 06:02:57 +0000

    add func find and New

--- route/table.go
+++ route/table.go
@@ -1,7 +1,8 @@
 package route
 
 import (
-	"fmt"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -24,12 +25,31 @@ type Table struct {
 	routes  map[string]Route
 }
 
+func New(ping int, gateway string, routes map[string]Route) Table {
+	table := Table{
+		ping:    ping,
+		gateway: gateway,
+		routes:  routes,
+	}
+	go checkNode(table)
+	return table
+}
+
 func checkNode(table Table) {
 	c := time.Tick(time.Duration(table.ping) * time.Second)
 	go func() {
+		now := time.Now().Second()
 		for {
 			<-c
-			fmt.Printf("11111")
+			removeKeys := make(map[string]bool)
+			for key, route := range table.routes {
+				if now-route.time >= table.ping {
+					removeKeys[key] = true
+				}
+			}
+			for key, _ := range removeKeys {
+				delete(table.routes, key)
+			}
 		}
 	}()
 }
@@ -53,4 +73,31 @@ func (table Table) add(routes []Route) {
 	for _, route := range routes {
 		table.routes[route.cidr] = route
 	}
+}
+
+func isBelong(ip string, cidr string) bool {
+	ipAddr := strings.Split(ip, `.`)
+	if len(ipAddr) < 4 {
+		return false
+	}
+	cidrArr := strings.Split(cidr, `/`)
+	if len(cidrArr) < 2 {
+		return false
+	}
+	var tmp = make([]string, 0)
+	for key, value := range strings.Split(`255.255.255.0`, `.`) {
+		iint, _ := strconv.Atoi(value)
+		iint2, _ := strconv.Atoi(ipAddr[key])
+		tmp = append(tmp, strconv.Itoa(iint&iint2))
+	}
+	return strings.Join(tmp, `.`) == cidrArr[0]
+}
+
+func (table Table) find(address string) string {
+	for _, route := range table.routes {
+		if isBelong(address, route.cidr) {
+			return route.target
+		}
+	}
+	return ""
 }
